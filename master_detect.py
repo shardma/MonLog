c7618771d29ba82a72b49647d9cd1e5c2f0b099a
Ayush Senehang <76719404+shardma@users.noreply.github.com>
2026-06-28 23:03:18 -0500

    added network detection json, need more research

--- master_detect.py
+++ master_detect.py
@@ -15,16 +15,15 @@ def main():
     print("Extracting events...")
     events = extract_events(LOG_PATH, MAX_RECORDS, wanted_event_ids=["1", "13"])
 
-    print("Running command line detection")
+    print("Running command line detection...")
     alerts = detect_commandline.detect(events)
     detect_commandline.print_alerts(alerts)
-    print("Done!")
 
     alerts = detect_parent_child.detect(events)
     detect_parent_child.print_alerts(alerts)
     print("Done!")
 
-    print("Running registry detection")
+    print("Running registry detection...")
     alerts = registry_persistence.detect(events)
     registry_persistence.print_alerts(alerts)
 
